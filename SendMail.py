5c9e2097400eafebb7d7d71c7fa6861a1fc3485f
Reaper-one <74783185+Reaper-one@users.noreply.github.com>
2020-12-24 18:01:57 +0000

    Update SendMail.py

--- SendMail.py
+++ SendMail.py
@@ -1,6 +1,6 @@
 import smtplib
-senderMail='justin.fuller15537@gmail.com'
-recieverMail='chitranjan15537@gmail.com'
+senderMail='***SENDERS MAIL*****'
+recieverMail='*****RECIEVERS MAIL******'
 body='Hello world!'
 
 #Create an obj for SMTP class, accepts 2 parameters, smtp address and port number
@@ -8,7 +8,7 @@ server=smtplib.SMTP('smtp.gmail.com',587)
 #to start a secure encrypted connection, sequence should be maintained
 server.starttls()
 #For loggin in to the mailbox, login function accpts two values as parameter, email and password
-server.login(senderMail,'storm_thunder')
+server.login(senderMail,'*****SENDERS MAIL PASSWORD*******')
 #To send a hello to the server (optional)
 server.ehlo()
 #sendmail() is for sendind the mail, accpts 3 values - Sender's mail ID, Reciever's mail ID, Body respectively
